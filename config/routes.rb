490d566a45a89145e25d7855ac52498ea2a8d298
chandana <aprchandana@gmail.com>
2020-06-03 14:56:52 +0530

    change

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
+  devise_for :users
       resources :users, only: [:new, :create]
           get 'login', to: 'sessions#new'
           post 'login', to: 'sessions#create'
           get 'welcome', to: 'sessions#welcome'
-        root to ‘sessions#welcome’
+        root to: "sessions#welcome"
 end
